e901cfa8da21db58381b960ebcd89e187c646ffd
TaoPan <TaoPan@DESKTOP-ITEVP7E>
2024-04-16 14:44:01 +0930

    update the method name

--- src/test/java/sit707_tasks/DateUtilTest.java
+++ src/test/java/sit707_tasks/DateUtilTest.java
@@ -66,6 +66,8 @@ public class DateUtilTest {
 		System.out.println(date);
 		date.increment();
 		System.out.println(date);
+		Assert.assertEquals(1, date.getMonth());
+        Assert.assertEquals(2, date.getDay());
 	}
 	
 	@Test
@@ -78,187 +80,162 @@ public class DateUtilTest {
 	}
 	
 	@Test
-	public void test1B(){
-		try {
+	public void testJuneminshouldincrementToJune2(){
+		
 		    System.out.println("June min: increment should be 2nd June.");
 		    DateUtil date = new DateUtil(1, 6, 1994);
 		    System.out.println(date);
 		    date.increment();
 		    System.out.println(date);
-		} catch (Exception e) {
-		    System.out.println("Exception caught: " + e.getMessage());
-		}
+		    Assert.assertEquals(6, date.getMonth());
+	        Assert.assertEquals(2, date.getDay());
+		
 	}
 
 	@Test
-	public void test2B(){
-		try {
-		    System.out.println("June min: increment should be 3rd June.");
-		    DateUtil date = new DateUtil(2, 6, 1994);
-		    System.out.println(date);
-		    date.increment();
-		    System.out.println(date);
-		} catch (Exception e) {
-		    System.out.println("Exception caught: " + e.getMessage());
-		}
+	public void testJuneMinShouldIncrementToJune3() {
+	    System.out.println("June min: increment should be 3rd June.");
+	    DateUtil date = new DateUtil(2, 6, 1994);
+	    System.out.println(date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(3, date.getDay());
+	}
+
+	@Test
+	public void testJuneMinShouldIncrementToJune16() {
+	    System.out.println("June min: increment should be 16th June.");
+	    DateUtil date = new DateUtil(15, 6, 1994);
+	    System.out.println(date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(16, date.getDay());
+	}
+
+	@Test
+	public void testJuneMinShouldIncrementToJuly1() {
+	    System.out.println("June min: increment should be 1st July.");
+	    DateUtil date = new DateUtil(30, 6, 1994);
+	    System.out.println(date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(7, date.getMonth());
+	    Assert.assertEquals(1, date.getDay());
+	}
+
+	@Test
+	public void testJuneMinShouldThrowException() {
+		
+	    System.out.println("June min: increment should be Invalid Date.");
+	    try {
+	    DateUtil date = new DateUtil(31, 6, 1994);
+	    
+	  	} catch (Exception e) {
+		System.out.println(e.getMessage());
+		Assert.assertEquals("Invalid day: 31, max day: 30", e.getMessage());
+	  	}
 	    
 	}
 
 	@Test
-	public void test3B(){
-		try {
-		    System.out.println("June min: increment should be 16th June.");
-		    DateUtil date = new DateUtil(15, 6, 1994);
-		    System.out.println(date);
-		    date.increment();
-		    System.out.println(date);
-		} catch (Exception e) {
-		    System.out.println("Exception caught: " + e.getMessage());
-		}
+	public void testJanuaryMinShouldIncrementToJanuary16() {
+	    System.out.println("January min: increment should be 16th January.");
+	    DateUtil date = new DateUtil(15, 1, 1994);
+	    System.out.println(date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(1, date.getMonth());
+	    Assert.assertEquals(16, date.getDay());
 	}
 
 	@Test
-	public void test4B(){
-		try {
-		    System.out.println("June min: increment should be 1st July.");
-		    DateUtil date = new DateUtil(30, 6, 1994);
-		    System.out.println(date);
-		    date.increment();
-		    System.out.println(date);
-		} catch (Exception e) {
-		    System.out.println("Exception caught: " + e.getMessage());
-		}
+	public void testFebruaryMinShouldIncrementToFebruary16() {
+	    System.out.println("February min: increment should be 16th February.");
+	    DateUtil date = new DateUtil(15, 2, 1994);
+	    System.out.println(date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(2, date.getMonth());
+	    Assert.assertEquals(16, date.getDay());
 	}
 
 	@Test
-	public void test5B(){
-	
-		try {
-		    System.out.println("June min: increment should be Invalid Date.");
-		    DateUtil date = new DateUtil(31, 6, 1994);
-		    System.out.println(date);
-		} catch (Exception e) {
-		    System.out.println("Exception caught: " + e.getMessage());
-		}
+	public void testNovemberMinShouldIncrementToNovember16() {
+	    System.out.println("November min: increment should be 16th November.");
+	    DateUtil date = new DateUtil(15, 11, 1994);
+	    System.out.println(date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(11, date.getMonth());
+	    Assert.assertEquals(16, date.getDay());
 	}
 
 	@Test
-	public void test6B(){
-		try {
-		    System.out.println("January min: increment should be 16th January.");
-		    DateUtil date = new DateUtil(15, 1, 1994);
-		    System.out.println(date);
-		    date.increment();
-		    System.out.println(date);
-		} catch (Exception e) {
-		    System.out.println("Exception caught: " + e.getMessage());
-		}
+	public void testDecemberMinShouldIncrementToDecember16() {
+	    System.out.println("December min: increment should be 16th December.");
+	    DateUtil date = new DateUtil(15, 12, 1994);
+	    System.out.println(date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(12, date.getMonth());
+	    Assert.assertEquals(16, date.getDay());
 	}
 
 	@Test
-	public void test7B(){
-		try {
-		    System.out.println("February min: increment should be 16th February.");
-		    DateUtil date = new DateUtil(15, 2, 1994);
-		    System.out.println(date);
-		    date.increment();
-		    System.out.println(date);
-		} catch (Exception e) {
-		    System.out.println("Exception caught: " + e.getMessage());
-		}
-
+	public void testJuneMinShouldIncrementToJune16InYear1700() {
+	    System.out.println("June min: increment should be 16th June.");
+	    DateUtil date = new DateUtil(15, 6, 1700);
+	    System.out.println(date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(16, date.getDay());
 	}
 
 	@Test
-	public void test8B(){
-		try {
-		    System.out.println("November min: increment should be 16th November.");
-		    DateUtil date = new DateUtil(15, 11, 1994);
-		    System.out.println(date);
-		    date.increment();
-		    System.out.println(date);
-		} catch (Exception e) {
-		    System.out.println("Exception caught: " + e.getMessage());
-		}
+	public void testJuneMinShouldIncrementToJune16InYear1701() {
+	    System.out.println("June min: increment should be 16th June.");
+	    DateUtil date = new DateUtil(15, 6, 1701);
+	    System.out.println(date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(16, date.getDay());
 	}
 
 	@Test
-	public void test9B(){
-		try {
-		    System.out.println("December min: increment should be 16th December.");
-		    DateUtil date = new DateUtil(15, 12, 1994);
-		    System.out.println(date);
-		    date.increment();
-		    System.out.println(date);
-		} catch (Exception e) {
-		    System.out.println("Exception caught: " + e.getMessage());
-		}
+	public void testJuneMinShouldIncrementToJune16InYear2023() {
+	    System.out.println("June min: increment should be 16th June.");
+	    DateUtil date = new DateUtil(15, 6, 2023);
+	    System.out.println(date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(16, date.getDay());
 	}
 
 	@Test
-	public void test10B(){
-		try {
-		    System.out.println("June min: increment should be 16th June.");
-		    DateUtil date = new DateUtil(15, 6, 1700);
-		    System.out.println(date);
-		    date.increment();
-		    System.out.println(date);
-		} catch (Exception e) {
-		    System.out.println("Exception caught: " + e.getMessage());
-		}
+	public void testJuneMinShouldIncrementToJune16InYear2024() {
+	    System.out.println("June min: increment should be 16th June.");
+	    DateUtil date = new DateUtil(15, 6, 2024);
+	    System.out.println(date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(6, date.getMonth());
+	    Assert.assertEquals(16, date.getDay());
 	}
 
 	@Test
-	public void test11B(){
-
-		try {
-		    System.out.println("June min: increment should be 16th June.");
-		    DateUtil date = new DateUtil(15, 6, 1701);
-		    System.out.println(date);
-		    date.increment();
-		    System.out.println(date);
-		} catch (Exception e) {
-		    System.out.println("Exception caught: " + e.getMessage());
-		}
-	}
-
-	@Test
-	public void test12B(){
-		try {
-		    System.out.println("June min: increment should be 16th June.");
-		    DateUtil date = new DateUtil(15, 6, 2023);
-		    System.out.println(date);
-		    date.increment();
-		    System.out.println(date);
-		} catch (Exception e) {
-		    System.out.println("Exception caught: " + e.getMessage());
-		}
-	}
-
-	@Test
-	public void test13B(){
-		try {
-		    System.out.println("June min: increment should be 16th June.");
-		    DateUtil date = new DateUtil(15, 6, 2024);
-		    System.out.println(date);
-		    date.increment();
-		    System.out.println(date);
-		} catch (Exception e) {
-		    System.out.println("Exception caught: " + e.getMessage());
-		}
-	}
-	
-	@Test
-	public void extraTestCase(){
-		try {
-		    System.out.println("Feb min: increment should be 29th Feb.");
-		    DateUtil date = new DateUtil(28, 2, 2024);
-		    System.out.println(date);
-		    date.increment();
-		    System.out.println(date);
-		} catch (Exception e) {
-		    System.out.println("Exception caught: " + e.getMessage());
-		}
+	public void testFebruaryMinShouldIncrementToFebruary29InLeapYear2024() {
+	    System.out.println("Feb min: increment should be 29th Feb.");
+	    DateUtil date = new DateUtil(28, 2, 2024);
+	    System.out.println(date);
+	    date.increment();
+	    System.out.println(date);
+	    Assert.assertEquals(2, date.getMonth());
+	    Assert.assertEquals(29, date.getDay());
 	}
 
 }
